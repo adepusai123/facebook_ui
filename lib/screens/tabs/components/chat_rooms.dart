25334f6fcf2a156a578e8aee9129760c59383d74
Sai Kumar <adepusai123@gmail.com>
2020-11-22 14:25:37 +0530

    chat rooms

--- lib/screens/tabs/components/chat_rooms.dart
+++ lib/screens/tabs/components/chat_rooms.dart
@@ -11,51 +11,64 @@ class ChatRoomsWidget extends StatefulWidget {
 
 class _ChatRoomsWidgetState extends State<ChatRoomsWidget> {
   List<dynamic> rooms = [
-    {"name": "SaiKumar Name"},
-    {"name": "Private Name"}
+    {"name": "SaiKumar Adepu", "image": "assets/images/sai_image.jpeg"},
+    {"name": "Private", "image": "assets/images/sai_image.jpeg"}
   ];
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 140,
+      height: 80,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          return InkWell(
-            onTap: () {
-              print("Room Index $index");
-            },
-            child: Container(
-              width: 90,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey[300]),
-              ),
-              margin: EdgeInsets.all(8),
-              padding: EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    child: Icon(
-                      Icons.account_circle,
-                      color: Colors.blue[400],
-                      size: 50,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 4,
-                  ),
-                  Text(
-                    rooms[index]['name'],
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                    overflow: TextOverflow.ellipsis,
-                  )
-                ],
-              ),
-            ),
-          );
+          return ChatRoom(room: rooms[index]);
         },
         itemCount: rooms.length,
+      ),
+    );
+  }
+}
+
+class ChatRoom extends StatelessWidget {
+  const ChatRoom({
+    Key key,
+    @required this.room,
+  }) : super(key: key);
+
+  final Map room;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        print("Room ${room['name']}");
+      },
+      child: Container(
+        width: 80,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey[300]),
+        ),
+        margin: EdgeInsets.all(5),
+        padding: EdgeInsets.all(5),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(5),
+              child: Icon(
+                Icons.account_circle,
+                color: Colors.blue[400],
+              ),
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            Text(
+              room['name'],
+              style: TextStyle(fontWeight: FontWeight.bold),
+              overflow: TextOverflow.ellipsis,
+            )
+          ],
+        ),
       ),
     );
   }
