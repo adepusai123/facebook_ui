d0d9aaa71b23afe905ad68ccdacc53d6bf1a5bb9
Sai Kumar <adepusai123@gmail.com>
2020-11-24 10:27:29 +0530

    Wall & profile adjustments done

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -15,36 +16,109 @@ class ProfileScreen extends StatelessWidget {
             color: Colors.black,
           ),
         ),
-        body: Container(
-          padding: EdgeInsets.all(10),
-          child: Stack(
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Container(
-                    height: 220,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage("assets/images/sai_img.jpeg"),
-                      ),
-                    ),
-                  ),
-                ],
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: size.height * 0.37,
+                padding: EdgeInsets.all(10),
+                child: WallProfileComponent(),
               ),
-              Positioned(
-                child: CircleAvatar(
-                  radius: 100,
-                  backgroundImage: AssetImage("assets/images/sai_img.jpeg"),
+              Text(
+                "Sai Kumar Adepu",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                 ),
-                left: (MediaQuery.of(context).size.width / 2) - 110,
-                top: 110,
               )
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class WallProfileComponent extends StatelessWidget {
+  const WallProfileComponent({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Column(
+          children: [
+            Container(
+              height: 220,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                ),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/images/sai_img.jpeg"),
+                ),
+              ),
+              child: Stack(
+                children: [
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey[300],
+                        child: Icon(
+                          Icons.photo_camera_rounded,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        Positioned(
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 6,
+                  ),
+                  borderRadius: BorderRadius.circular(120),
+                ),
+                child: CircleAvatar(
+                  radius: 100,
+                  backgroundImage: AssetImage("assets/images/sai_img.jpeg"),
+                ),
+              ),
+              Positioned(
+                  bottom: 18,
+                  right: 0,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.grey[300],
+                    child: Icon(
+                      Icons.photo_camera_rounded,
+                      color: Colors.black,
+                    ),
+                  ))
+            ],
+          ),
+          left: (MediaQuery.of(context).size.width / 2) - 110,
+          top: 110,
+        ),
+      ],
     );
   }
 }
